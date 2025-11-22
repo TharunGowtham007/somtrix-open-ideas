959266d54752647987ca6e9dc4900e86a730fedb
Tharun Gujjala <tharungowtham2006@gmail.com>
2025-11-23 03:24:49 +0530

    Use dynamic PORT and robust server for Render

--- server.js
+++ server.js
@@ -152,6 +152,8 @@ app.get("*", (req, res) => {
 });
 
 // Start server
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`SomTriX ideas board listening on port ${PORT}`);
 });
